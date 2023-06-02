40bf1ee2e5b8266ab56d52b1e80f1422de9e66c3
Lidia <lidiayang104@gmail.com>
2023-06-02 21:57:51 +0200

    cambio de value de select y archivo data creado

---
+++ js/data.js
@@ -0,0 +1,15 @@
+tareas = [{
+    id: 1,
+    contenido: 'poner lavadora',
+    nivel: 'urgente'
+},
+{
+    id: 2,
+    contenido: 'hacer compra',
+    nivel: 'diaria'
+},
+{
+    id: 3,
+    contenido: 'hacer limpieza de casa',
+    nivel: 'mensual'
+}]
