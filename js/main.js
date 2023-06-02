503d7b0469478ca48ced7f0eb80d537b7dc75106
Lidia <lidiayang104@gmail.com>
2023-06-03 00:14:39 +0200

    funcunnalidad de filtrar terminada

--- js/main.js
+++ js/main.js
@@ -49,9 +49,11 @@ function addTarea(event) {
     }
 
     // 给新加的事件添加id的问题
+    // 不能添加相同的事件
 
     tareas.push(newTarea)
     console.log(newTarea);
+    console.log(tareas);
 
     // resetear los contenidos de input y select
     sectionTarea.innerHTML = ""
@@ -62,4 +64,37 @@ function addTarea(event) {
 }
 
 btnAdd.addEventListener('click', addTarea)
-// nivelAdd.addEventListener('change',addTarea)
+
+// filtrar 
+let filtarBtn = document.querySelector('.filter button');
+let tareaFilter = document.querySelector('.filter input');
+let nivelFilter = document.querySelector('.filter #nivel-filter')
+
+
+function filterTarea(event) {
+
+    let tareaFiltrada = []
+
+    if (nivelFilter.value === "" && tareaFilter.value === "") {
+        alert("Por favor, selecciona el método de filtrado");
+        return;
+    } else if (nivelFilter.value === "") {
+        tareaFiltrada = tareas.filter(tarea => tarea.contenido.toLowerCase().includes(tareaFilter.value.toLowerCase()))
+    } else {
+        tareaFiltrada = tareas.filter(tarea => tarea.nivel === nivelFilter.value)
+    }
+
+    console.log(tareaFiltrada);
+
+    sectionTarea.innerHTML = ""
+    printAllTarea(tareaFiltrada, sectionTarea)
+
+    // resetear los contenidos de input y select
+    tareaFilter.value = ""
+    nivelFilter.value = ""
+}
+
+
+
+
+filtarBtn.addEventListener('click', filterTarea)
